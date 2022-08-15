67f72eb95a23c6c046cef404ae9d128a00771c2b
Gbemi-Emmanuel <109102914+Gbemi-Emmanuel@users.noreply.github.com>
2022-08-16 00:12:57 +0100

    js updated

--- script/main.js
+++ script/main.js
@@ -24,34 +24,80 @@
 // }
 
 HandleSubmit = (collector) => {
+
     //firstname validation
     if (collector.firstname.value.length < 6) {
         document.getElementById("fName").style.border = "1px solid tomato";
+        document.getElementById("fnMessage").innerHTML = "Firstname should be more than 6 characters!";
+        document.getElementById("fnMessage").style.display = "block";
         
     }
     else{
-        document.getElementById("fName").style.border = "1px solid lightblue"
+        document.getElementById("fName").style.border = "1px solid lightblue";
+        document.getElementById("fnMessage").style.display = "none";
         console.log(collector.firstname.value)
     }
 
     //lastname validation
     if (collector.lastname.value.length < 5) {
         document.getElementById("lName").style.border = "1px solid tomato";
+        document.getElementById("lnMessage").innerHTML = "Lastname should be more than 5 characters!"
+        document.getElementById("fnMessage").style.display = "block";
         
     }
     else{
-        document.getElementById("lName").style.border = "1px solid lightblue"
+        document.getElementById("lName").style.border = "1px solid lightblue";
+        document.getElementById("lnMessage").style.display = "none";
         console.log(collector.lastname.value)
     }
 
-    //password validation
-    if (collector.lastname.value.length < 5) {
-        document.getElementById("lName").style.border = "1px solid tomato";
-        
+    //password Validation
+    if (collector.password.value.length <= 8) {
+        document.getElementById("password").style.border = "1px solid tomato";
+        document.getElementById("pwdMessage").innerHTML = "Password should be 8 or above!";
+        document.getElementById("pwdMessage").style.display = "block";
     }
     else{
-        document.getElementById("lName").style.border = "1px solid lightblue"
-        console.log(collector.lastname.value)
+        document.getElementById("password").style.border = "1px solid lightblue";
+        document.getElementById("pwdMessage").style.display = "none";
+        console.log(collector.password.value)
+    }
+
+    //Confirm Password Validation
+    if (collector.password.value !== collector.cPassword.value ) {
+        document.getElementById("cPassword").style.border = "1px solid tomato";
+        document.getElementById("cPwdMessage").innerHTML = "Password Incorrect!";
+        document.getElementById("cPwdMessage").style.display = "block";
+    }
+    else{
+        document.getElementById("cPassword").style.border = "1px solid lightblue";
+        document.getElementById("cPwdMessage").style.display = "none";
+        console.log(collector.cPassword.value)
+    }
+
+
+    //Phone Number Validation
+    if (collector.phoneNo.value.length <= 11) {
+        document.getElementById("phoneNo").style.border = "1px solid tomato";
+        document.getElementById("pnMessage").innerHTML = "Invalid! Number";
+        document.getElementById("pnMessage").style.display = "block";
+    }
+    else{
+        document.getElementById("phoneNo").style.border = "1px solid lightblue";
+        document.getElementById("pnMessage").style.display = "none";
+        console.log(collector.phoneNo.value)
+    }
+
+    //Email Vaildation
+    if (collector.email.value != includes("@")) {
+        document.getElementById("email").style.border = "1px solid tomato";
+        document.getElementById("emailMessage").innerHTML = "Invalid! Email Address";
+        document.getElementById("emailMessage").style.display = "block";
+    }
+    else{
+        document.getElementById("email").style.border = "1px solid lightblue";
+        document.getElementById("emailMessage").style.display = "none";
+        console.log(collector.email.value)
     }
 };
 
